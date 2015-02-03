5e2740995fc9acc3479cc972339550f197c2d247
Delilah Dunn <delilah@digitalore.com>
2015-02-03 04:22:22 -0500

    updated testing field for uploading image to canvas, word wrapping of text, and cleaner coordinates

--- static/FabricTest/js/canvas.js
+++ static/FabricTest/js/canvas.js
@@ -1,35 +1,28 @@
-// Run only when HTML is loaded and
-// DOM properly initialized (courtesy jquery)
 $(function () {
 
-    // Obtain a canvas drawing surface from fabric.js
     var canvas = new fabric.Canvas('c');
-
-    // Create a text object.
-    // Does not display it-the canvas doesn't
-    // know about it yet.
+    //Rect for Images
     var pic = new fabric.Rect({
         left: 0,
         top: 0,
         fill: 'green',
-        width: 100,
-        height: 100,
+        width: 480,
+        height: 220,
         opacity: 0.8
 
     });
-
+    //Rect for Text
     var text = new fabric.Rect({
         left: 0,
         top: pic.height,
         fill: 'blue',
-        width: 100,
-        height: 100,
+        width: 480,
+        height: 220,
         opacity: 0.8
     });
 
-    // Attach it to the canvas object, then (re)display
-    // the canvas.
 
+    //on click add objects
     $("#addPic").click(function(){
         //remove if exist then add
         canvas.remove(pic);
@@ -42,40 +35,85 @@ $(function () {
         canvas.add(text);
     });
 
-    pic.on('moving', function() {
+    //observe any mouse movement
+    canvas.observe('mouse:move', function() {
         $("#contents").html(
             '<div class=\"pic\">'+
-            'Pic Top: ' + pic.top + '<br/>' +
-            'Pic Left: ' + pic.left + '<br/>' +
-            'Pic Width: ' + pic.getWidth() + '<br/>' +
-            'Pic Height: ' + pic.getHeight() + '<br/><br/>' +
+            'Pic Top: ' + Math.ceil(pic.top) + '<br/>' +
+            'Pic Left: ' + Math.ceil(pic.left) + '<br/>' +
+            'Pic Width: ' + Math.ceil(pic.getWidth()) + '<br/>' +
+            'Pic Height: ' + Math.ceil(pic.getHeight()) + '<br/><br/>' +
             '</div><div class=\"text\">'+
-            'Text Top: ' + text.top + '<br/>' +
-            'Text Left: ' + text.left + '<br/>' +
-            'Text Width: ' + text.getWidth() + '<br/>' +
-            'Text Height: ' + text.getHeight() + '<br/>'
+            'Text Top: ' + Math.ceil(text.top) + '<br/>' +
+            'Text Left: ' + Math.ceil(text.left) + '<br/>' +
+            'Text Width: ' + Math.ceil(text.getWidth()) + '<br/>' +
+            'Text Height: ' + Math.ceil(text.getHeight()) + '<br/>'
             + '</div>'
         )
     });
 
-    text.on('moving', function() {
-        $("#contents").html(
-            '<div class=\"pic\">'+
-            'Pic Top: ' + pic.top + '<br/>' +
-            'Pic Left: ' + pic.left + '<br/>' +
-            'Pic Width: ' + pic.getWidth() + '<br/>' +
-            'Pic Height: ' + pic.getHeight() + '<br/><br/>' +
-            '</div><div class=\"text\">'+
-            'Text Top: ' + text.top + '<br/>' +
-            'Text Left: ' + text.left + '<br/>' +
-            'Text Width: ' + text.getWidth() + '<br/>' +
-            'Text Height: ' + text.getHeight() + '<br/>'
-            + '</div>'
-        )
-    });
-
+    //remove selected object
     $("#removeObject").click(function(){
         var activeO = canvas.getActiveObject();
         if(!activeO) return;
         canvas.remove(activeO) });
+
+ //TODO: upload and resize image (optional)
+ //TODO: BIG -- Apply coordinate data to product
+
+$( "#uploadImg" ).change(function(e) {
+    var reader = new FileReader();
+    reader.onload = function (event) { console.log('fdsf');
+        var imgObj = new Image();
+        imgObj.src = event.target.result;
+        imgObj.onload = function () {
+
+            var image = new fabric.Image(imgObj);
+            image.set({
+                left: 0,
+                top: 0,
+                padding: 0,
+                cornersize: 0,
+                lockUniScaling: true,
+                lockRotation: true
+            });
+            //image.scale(getRandomNum(0.1, 0.25)).setCoords();
+            canvas.add(image);
+        }
+
+    }
+    reader.readAsDataURL(e.target.files[0]);
+});
+
+//Download canvas output to file (Placeholder)
+    function download(url,name){
+    //make the link. set the href and download. emulate dom click
+        $('<a>').attr({href:url,download:name})[0].click();
+    }
+    function downloadFabric(canvas,name){
+    //convert the canvas to a data url and download it.
+        download(canvas.toDataURL(),name+'.png');
+    }
+    //download should be productname or id
+    $("#download").click(function(){
+    downloadFabric(canvas,'download');});
+
+
+//NOTE: For applying text to canvas
+//TODO: Fonts should be placed here
+    var unformatted = new fabric.Text('this will be text that multi-line using wordwrap.js', {
+        left: 5, //Take the block's position
+        top: 5,
+        fill: 'black',
+        fontFamily: 'Lato',
+        fontSize: 25
+    });
+//canvas.add(unformatted); //--testing
+
+//need to fit within coordinates
+//TODO: change height and with to textcoord h/w and font selection
+    var formatted = wrapCanvasText(unformatted, canvas, 200, 180, 'left');
+    formatted.top = 50;
+    formatted.left = 5;
+   // canvas.add(formatted);
 });
