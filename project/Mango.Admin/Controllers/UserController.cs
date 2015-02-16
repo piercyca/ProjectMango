b841653ca221b66749c41672f511b79ea2ed27c5
jkadaks <jkadaks@gmail.com>
2015-02-15 20:58:34 -0500

    Added Users view and controller

---
+++ project/Mango.Admin/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mango.Admin.Models;
+using Mango.Core.Data;
+
+namespace Mango.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            var app = new ApplicationDbContext();
+            var users = app.Users.Select(s => s);
+            var context = new Mango.Core.Data.MangoContext();
+            //var products = context.Products.Select(s => s);
+            //var users = context.
+            return View(users);
+        }
+
+       
+    }
+}
