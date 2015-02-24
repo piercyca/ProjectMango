2798d01d7df296b330eaac962bbc7aa5978c1165
craigmoliver <craigmoliver@gmail.com>
2015-02-24 14:04:55 -0500

    Nuget updates
    Signed-off-by: craigmoliver <craigmoliver@gmail.com>

---
+++ project/Mango.Admin/Models/ProductCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mango.Admin.Models
+{
+    public class ProductCategoryViewModel
+    {
+    }
+}
