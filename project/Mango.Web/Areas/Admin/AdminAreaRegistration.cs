def8e056b7ddb4f0b45ea9a3f0c543774ac25632
craigmoliver <craigmoliver@gmail.com>
2015-03-03 15:38:43 -0500

    RouteConfig updates - Add "Home" to "Admin" area
    Signed-off-by: craigmoliver <craigmoliver@gmail.com>

--- project/Mango.Web/Areas/Admin/AdminAreaRegistration.cs
+++ project/Mango.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace Mango.Web.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "Mango.Web.Areas.Admin.Controllers" }
             );
         }
     }
