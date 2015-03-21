aade04aeb038a2940624736637f75956d2b4d10d
craigmoliver <craigmoliver@gmail.com>
2015-03-21 12:57:15 -0400

    Change Rollup

---
+++ project/Mango.Core.Web/CustomValidators/UrlSlugValidatorAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Mango.Core.Web.CustomValidators {
+
+    /// <summary>
+    /// Validates url slug value.
+    /// </summary>
+    public class UrlSlugValidatorAttribute: RegularExpressionAttribute, IClientValidatable {
+
+        private const string _pattern = @"[a-z0-9-_]*";
+        private const string _errorMessage = "Only Letters, Numbers, and &quot;-&quot; allowed";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UrlSlugValidatorAttribute"/> class.
+        /// </summary>
+        public UrlSlugValidatorAttribute()
+            : base(_pattern) {
+            ErrorMessage = _errorMessage;
+        }
+
+        /// <summary>
+        /// When implemented in a class, returns client validation rules for that class.
+        /// </summary>
+        /// <param name="metadata">The model metadata.</param>
+        /// <param name="context">The controller context.</param>
+        /// <returns>
+        /// The client validation rules for this validator.
+        /// </returns>
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context) {
+            yield return new ModelClientValidationRegexRule(_errorMessage, _pattern);
+        }
+    }
+
+    
+}
---
+++ project/Mango.Web/ViewModels/ProductCategoryFormViewModel.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using Mango.Core.Entity;
+using Mango.Core.Web.CustomValidators;
+
+namespace Mango.Web.ViewModels
+{
+    /// <summary>
+    /// View model for <see cref="ProductCategory" />
+    /// </summary>
+    public class ProductCategoryFormViewModel
+    {
+        [Required]
+        [HiddenInput(DisplayValue = false)]
+        public int ProductCategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [Required]
+        [UniqueValidator("ProductCategoryId", UniqueValidatorType.ProductCategoryUrlSlug, ErrorMessage = "Url Used")]
+        [UrlSlugValidator]
+        public string UrlSlug { get; set; }
+
+        [Required]
+        [RegularExpression("^(?:(?<Item>[a-z0-9\\s]+),)+(?<LastItem>[a-z0-9\\s]+)[\r\n]*$", ErrorMessage = "Comma separated list of lowercase words. Example: dog,cat,puppy,whale shark")]
+        public string Keywords { get; set; }
+    }
+}
