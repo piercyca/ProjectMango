c535da73486ee2eb8187654f2eae120a746dccea
craigmoliver <craigmoliver@gmail.com>
2015-03-22 19:55:43 -0400

    Azure.Storage stuff

---
+++ project/Mango.Storage/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Storage
+{
+    public class Image
+    {
+        private string _connectionString { get; set; }
+
+        public Image()
+        {
+            _connectionString = ConfigurationManager.AppSettings["ImageBlobConnectionString"];
+        }
+
+        public static void Storage()
+        {
+        }
+    }
+}
