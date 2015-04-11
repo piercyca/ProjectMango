ab6992ba818fa0d6ddc88474436723bc8b0f582c
jkadaks <jkadaks@gmail.com>
2015-04-10 23:50:03 -0400

    Added Seed data for Orders, OrderLineItems, and Addresses

--- project/Mango.Core/Migrations/Configuration.cs
+++ project/Mango.Core/Migrations/Configuration.cs
@@ -41,6 +41,16 @@ namespace Mango.Core.Migrations
             context.OrganizationImages.AddOrUpdate(pi => new { pi.OrganizationId, pi.SortOrder }, new OrganizationImage { OrganizationId = 1, SortOrder = 0, Url = "https://mangoassets.blob.core.windows.net/images/03b46d6e7ce3447fba1868358a9a69a5.jpg" });
             context.OrganizationImages.AddOrUpdate(pi => new { pi.OrganizationId, pi.SortOrder }, new OrganizationImage { OrganizationId = 1, SortOrder = 1, Url = "https://mangoassets.blob.core.windows.net/images/0a1baf98f04a4f96b78ebeaa38880415.jpg" });
 
+			//Orders
+			context.Orders.AddOrUpdate(o => o.OrderId, new Entity.Order {OrderId = 1, CustomerId = 1, ShipAddressId = 1, BillAddressId = 2, TotalAmount = 100.00m, DateCreated = DateTime.Now});
+
+			//Order Line Items
+			context.OrderLineItems.AddOrUpdate(ol => ol.OrderId, new Entity.OrderLineItem { OrderId = 1, OrderItemSequence = 1, ProductId = 1, UnitPrice = 22.50m, Quantity = 2, Configuration = "{\"layout\":{\"pic\":{\"top\":112,\"left\":111,\"width\":297,\"height\":136},\"text\":{\"top\":294,\"left\":133,\"width\":248,\"height\":114}}}" });
+			context.OrderLineItems.AddOrUpdate(ol => ol.OrderId, new Entity.OrderLineItem { OrderId = 1, OrderItemSequence = 2, ProductId = 2, UnitPrice = 15.50m, Quantity = 5, Configuration = "" });
+
+			//Adresses
+			context.Addresses.AddOrUpdate(a => a.AddressId, new Entity.Address { AddressId = 1, Status = (AddressStatus) 1, AddressType = (AddressType) 1, FirstName = "John", LastName = "Smith", Phone = "5555555555", AddressLine1 = "33 Main St.", AddressLine2 = "Apt. 2", City = "Athens", State = "GA", Zip = "30605", Country = "USA", DateCreated = DateTime.Now});
+			context.Addresses.AddOrUpdate(a => a.AddressId, new Entity.Address { AddressId = 2, Status = (AddressStatus) 1, AddressType = (AddressType) 2, FirstName = "John", LastName = "Smith", Phone = "7777777777", AddressLine1 = "157 Broad Ave.", AddressLine2 = "", City = "Atlanta", State = "GA", Zip = "30303", Country = "USA", DateCreated = DateTime.Now });
         }
     }
 }
